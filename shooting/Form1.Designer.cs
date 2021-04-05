545cbfff196428281585b8c3f68b6a5de674e4a0
GlobusOffZeWorld <gleb737.gl@gmail.com>
2021-04-05 11:23:05 +0300

    make task

---
+++ shooting/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace shooting {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.textBoxInputRadius = new System.Windows.Forms.TextBox();
+      this.labelHitRate = new System.Windows.Forms.Label();
+      this.labelMissRate = new System.Windows.Forms.Label();
+      this.labelHitName = new System.Windows.Forms.Label();
+      this.labelMissName = new System.Windows.Forms.Label();
+      this.labelInputRadius = new System.Windows.Forms.Label();
+      this.buttonNewGame = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // textBoxInputRadius
+      // 
+      this.textBoxInputRadius.Location = new System.Drawing.Point(12, 31);
+      this.textBoxInputRadius.Name = "textBoxInputRadius";
+      this.textBoxInputRadius.Size = new System.Drawing.Size(100, 20);
+      this.textBoxInputRadius.TabIndex = 0;
+      this.textBoxInputRadius.TextChanged += new System.EventHandler(this.textBoxInputRadius_TextChanged);
+      // 
+      // labelHitRate
+      // 
+      this.labelHitRate.Location = new System.Drawing.Point(12, 86);
+      this.labelHitRate.Name = "labelHitRate";
+      this.labelHitRate.Size = new System.Drawing.Size(100, 23);
+      this.labelHitRate.TabIndex = 1;
+      this.labelHitRate.Text = "0";
+      // 
+      // labelMissRate
+      // 
+      this.labelMissRate.Location = new System.Drawing.Point(12, 142);
+      this.labelMissRate.Name = "labelMissRate";
+      this.labelMissRate.Size = new System.Drawing.Size(100, 23);
+      this.labelMissRate.TabIndex = 1;
+      this.labelMissRate.Text = "0";
+      // 
+      // labelHitName
+      // 
+      this.labelHitName.Location = new System.Drawing.Point(12, 63);
+      this.labelHitName.Name = "labelHitName";
+      this.labelHitName.Size = new System.Drawing.Size(100, 23);
+      this.labelHitName.TabIndex = 1;
+      this.labelHitName.Text = "Hit count:";
+      // 
+      // labelMissName
+      // 
+      this.labelMissName.Location = new System.Drawing.Point(12, 119);
+      this.labelMissName.Name = "labelMissName";
+      this.labelMissName.Size = new System.Drawing.Size(100, 23);
+      this.labelMissName.TabIndex = 1;
+      this.labelMissName.Text = "Miss count";
+      // 
+      // labelInputRadius
+      // 
+      this.labelInputRadius.Location = new System.Drawing.Point(12, 5);
+      this.labelInputRadius.Name = "labelInputRadius";
+      this.labelInputRadius.Size = new System.Drawing.Size(100, 23);
+      this.labelInputRadius.TabIndex = 1;
+      this.labelInputRadius.Text = "Input radius";
+      // 
+      // buttonNewGame
+      // 
+      this.buttonNewGame.Location = new System.Drawing.Point(12, 168);
+      this.buttonNewGame.Name = "buttonNewGame";
+      this.buttonNewGame.Size = new System.Drawing.Size(75, 23);
+      this.buttonNewGame.TabIndex = 2;
+      this.buttonNewGame.Text = "NewGame";
+      this.buttonNewGame.UseVisualStyleBackColor = true;
+      this.buttonNewGame.Click += new System.EventHandler(this.buttonNewGame_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1200, 900);
+      this.Controls.Add(this.buttonNewGame);
+      this.Controls.Add(this.labelMissRate);
+      this.Controls.Add(this.labelMissName);
+      this.Controls.Add(this.labelInputRadius);
+      this.Controls.Add(this.labelHitName);
+      this.Controls.Add(this.labelHitRate);
+      this.Controls.Add(this.textBoxInputRadius);
+      this.Name = "Form1";
+      this.Text = "Target";
+      this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Mouse_Click);
+      this.Resize += new System.EventHandler(this.Form1_Resize);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+    }
+
+    private System.Windows.Forms.Button buttonNewGame;
+
+    private System.Windows.Forms.Label labelInputRadius;
+
+    private System.Windows.Forms.Label labelHitName;
+    private System.Windows.Forms.Label labelMissName;
+
+    private System.Windows.Forms.Label labelHitRate;
+    private System.Windows.Forms.Label labelMissRate;
+
+    private System.Windows.Forms.TextBox textBoxInputRadius;
+
+    #endregion
+  }
+}
---
+++ shooting/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace shooting {
+  public partial class Form1 : Form {
+    private Point[] _shotsArr = new Point[50];
+    private int _radius = 400;
+    private int _pointIndex = 0;
+    private int _hitRate = 0;
+    private int _missRate = 0;
+
+    private int H = 1200;
+    private int W = 900;
+
+    public Form1() {
+      InitializeComponent();
+    }
+
+    public int Radius {
+      get => _radius;
+      set => _radius = value;
+    }
+
+    protected override void OnPaint(PaintEventArgs e) {
+      base.OnPaint(e);
+      DrawTarget(ClientRectangle, e.Graphics);
+      DrawShots(e.Graphics);
+    }
+
+    private void Mouse_Click(object sender, MouseEventArgs e) {
+      Point shotCoordinates = new Point(MousePosition.X - Location.X - 12, MousePosition.Y - Location.Y - 35);
+      _shotsArr[_pointIndex] = shotCoordinates;
+      ++_pointIndex;
+
+      if (_pointIndex == 50) {
+        _pointIndex = 0;
+      }
+
+      Point hitCoordinates =
+        new Point(shotCoordinates.X + 5, shotCoordinates.Y + 5); // we need center of point, where diameter 10
+      HitCheck(hitCoordinates);
+
+      labelHitRate.Text = _hitRate.ToString();
+      labelMissRate.Text = _missRate.ToString();
+
+      Invalidate();
+    }
+
+    private void HitCheck(Point shot) {
+      Point centralPoint = new Point(W / 2, H / 2);
+
+      if (shot.Y < centralPoint.Y &&
+          Math.Sqrt(Math.Pow(shot.X - centralPoint.X, 2) + Math.Pow(shot.Y - centralPoint.Y, 2)) < Radius) {
+        ++_hitRate;
+      } else if (shot.Y > centralPoint.Y && ((centralPoint.X - _radius < shot.X && shot.X < centralPoint.X) &&
+                                             (centralPoint.Y < shot.Y && shot.Y < centralPoint.Y + _radius) &&
+                                             (centralPoint.X - shot.X < shot.Y - centralPoint.Y))) {
+        ++_hitRate;
+      } else {
+        ++_missRate;
+      }
+    }
+
+    private void DrawShots(Graphics g) {
+      SolidBrush brush = new SolidBrush(Color.Blue);
+      for (int i = 0; i < _shotsArr.Length; ++i) {
+        if (_shotsArr[i].X == 0 && _shotsArr[i].Y == 0) {
+          break;
+        }
+
+        g.FillEllipse(brush, _shotsArr[i].X, _shotsArr[i].Y, 10, 10);
+      }
+    }
+
+    private void DrawTarget(Rectangle r, Graphics g) {
+      g.Clear(Color.White);
+
+      H = r.Height;
+      W = r.Width;
+
+      int diameter = Radius * 2;
+
+      Point centralPoint = new Point(W / 2, H / 2);
+
+      SolidBrush brush = new SolidBrush(Color.Red);
+      Pen pen = new Pen(brush);
+
+      Point rightTopTrianglePoint = new Point(centralPoint.X, centralPoint.Y);
+      Point rightBottomTrianglePoint = new Point(centralPoint.X, centralPoint.Y + Radius);
+      Point leftBottomTrianglePoint = new Point(centralPoint.X - Radius, centralPoint.Y + Radius);
+
+      Point[] triangle = new Point[3];
+
+      triangle[0] = rightTopTrianglePoint;
+      triangle[1] = rightBottomTrianglePoint;
+      triangle[2] = leftBottomTrianglePoint;
+
+      Rectangle rect = new Rectangle(centralPoint.X - Radius, centralPoint.Y - Radius, diameter, diameter);
+      g.FillPie(brush, rect, 0, -180);
+
+      g.FillPolygon(brush, triangle);
+
+      brush.Dispose();
+      pen.Dispose();
+    }
+
+    private void ResetTarget() {
+      for (int i = 0; i < _shotsArr.Length; ++i) {
+        _shotsArr[i] = new Point(0, 0);
+        _pointIndex = 0;
+      }
+
+      _hitRate = 0;
+      _missRate = 0;
+
+      labelHitRate.Text = _hitRate.ToString();
+      labelMissRate.Text = _missRate.ToString();
+    }
+
+    private void Form1_Resize(object sender, EventArgs e) {
+      ResetTarget();
+
+      Invalidate();
+    }
+
+    private void textBoxInputRadius_TextChanged(object sender, EventArgs e) {
+      ResetTarget();
+      if (textBoxInputRadius.Text != "" && !textBoxInputRadius.Text.Contains("-") &&
+          Convert.ToInt32(textBoxInputRadius.Text) < 10000) {
+        Radius = Convert.ToInt32(textBoxInputRadius.Text);
+        Invalidate();
+      }
+    }
+
+    private void buttonNewGame_Click(object sender, EventArgs e) {
+      ResetTarget();
+      Invalidate();
+    }
+  }
+}
