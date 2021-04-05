410019c84aaeb0738546eed39795d8a353f535ee
GlobusOffZeWorld <gleb737.gl@gmail.com>
2021-04-05 10:48:58 +0300

    make task

---
+++ foolWinForms/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿using System.Windows.Forms;
+
+namespace foolWinForms {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.labelAreYouFool = new System.Windows.Forms.Label();
+      this.buttonYes = new System.Windows.Forms.Button();
+      this.buttonNo = new System.Windows.Forms.Button();
+      this.textBoxForCheatCode = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // labelAreYouFool
+      // 
+      this.labelAreYouFool.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+      this.labelAreYouFool.Location = new System.Drawing.Point(284, 9);
+      this.labelAreYouFool.Name = "labelAreYouFool";
+      this.labelAreYouFool.Size = new System.Drawing.Size(234, 58);
+      this.labelAreYouFool.TabIndex = 0;
+      this.labelAreYouFool.Text = "Are you fool?";
+      this.labelAreYouFool.MouseMove += new System.Windows.Forms.MouseEventHandler(this.textBoxForCheatCode_MouseMove);
+      // 
+      // buttonYes
+      // 
+      this.buttonYes.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.buttonYes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+      this.buttonYes.Location = new System.Drawing.Point(284, 56);
+      this.buttonYes.Name = "buttonYes";
+      this.buttonYes.Size = new System.Drawing.Size(104, 70);
+      this.buttonYes.TabIndex = 1;
+      this.buttonYes.TabStop = false;
+      this.buttonYes.Text = "Yes";
+      this.buttonYes.UseVisualStyleBackColor = true;
+      this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
+      this.buttonYes.MouseMove += new System.Windows.Forms.MouseEventHandler(this.buttonYes_MouseMove);
+      // 
+      // buttonNo
+      // 
+      this.buttonNo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.buttonNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+      this.buttonNo.Location = new System.Drawing.Point(394, 56);
+      this.buttonNo.Name = "buttonNo";
+      this.buttonNo.Size = new System.Drawing.Size(104, 70);
+      this.buttonNo.TabIndex = 1;
+      this.buttonNo.TabStop = false;
+      this.buttonNo.Text = "NO!!!";
+      this.buttonNo.UseVisualStyleBackColor = true;
+      this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
+      // 
+      // textBoxForCheatCode
+      // 
+      this.textBoxForCheatCode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+      this.textBoxForCheatCode.Location = new System.Drawing.Point(284, 166);
+      this.textBoxForCheatCode.Name = "textBoxForCheatCode";
+      this.textBoxForCheatCode.Size = new System.Drawing.Size(214, 13);
+      this.textBoxForCheatCode.TabIndex = 1;
+      this.textBoxForCheatCode.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1280, 720);
+      this.Controls.Add(this.textBoxForCheatCode);
+      this.Controls.Add(this.buttonNo);
+      this.Controls.Add(this.buttonYes);
+      this.Controls.Add(this.labelAreYouFool);
+      this.Name = "Form1";
+      this.Text = "Are you fool?";
+      this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+    }
+
+    private System.Windows.Forms.TextBox textBoxForCheatCode;
+
+    private System.Windows.Forms.Button buttonNo;
+    private System.Windows.Forms.Button buttonYes;
+
+    private System.Windows.Forms.Label labelAreYouFool;
+
+    #endregion
+  }
+}
---
+++ foolWinForms/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace foolWinForms {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+      buttonNo.BringToFront();
+    }
+
+    private const string SuperSecretCheatCode = "imnotfool";
+    private static string _inputKeys = "";
+    private static bool _buttonStop = false;
+
+    public static string InputKeys {
+      get => _inputKeys;
+      set => _inputKeys = value;
+    }
+
+    public static bool ButtonStop {
+      get => _buttonStop;
+      set => _buttonStop = value;
+    }
+
+    private void MoveButtonNo() {
+      if (ButtonStop) {
+        return;
+      }
+
+      const int buttonSpeed = 16;
+
+      int buttonNoDimensionX = buttonNo.Location.X + 52 - MousePosition.X + Location.X;
+      int buttonNoDimensionY = buttonNo.Location.Y + 35 - MousePosition.Y + Location.Y;
+
+      MoveX(buttonNoDimensionX, buttonNoDimensionY, buttonSpeed);
+      MoveY(buttonNoDimensionY, buttonNoDimensionX, buttonSpeed);
+
+      Random rnd = new Random();
+      int randomCoordinatesX = rnd.Next(20, this.Size.Width - 176);
+      int randomCoordinatesY = rnd.Next(20, this.Size.Height - 125);
+
+      if (buttonNo.Location.X <= 20 || buttonNo.Location.X >= this.Size.Width - 176) {
+        buttonNo.Location = new Point(randomCoordinatesX, randomCoordinatesY);
+      }
+
+      if (buttonNo.Location.Y <= 20 || buttonNo.Location.Y >= this.Size.Height - 125) {
+        buttonNo.Location = new Point(randomCoordinatesX, randomCoordinatesY);
+      }
+    }
+
+    private void MoveX(int buttonNoDimensionX, int buttonNoDimensionY, int buttonSpeed) {
+      if (buttonNoDimensionX >= 0 && buttonNoDimensionX - 124 <= 0 && Math.Abs(buttonNoDimensionY) - 70 <= 0) {
+        buttonNo.Left += buttonSpeed;
+      } else if (buttonNoDimensionX <= 0 && buttonNoDimensionX + 124 >= 0 && Math.Abs(buttonNoDimensionY) - 70 <= 0) {
+        buttonNo.Left -= buttonSpeed;
+      }
+    }
+
+    private void MoveY(int buttonNoDimensionY, int buttonNoDimensionX, int buttonSpeed) {
+      if (buttonNoDimensionY >= 0 && buttonNoDimensionY - 90 <= 0 && Math.Abs(buttonNoDimensionX) - 104 <= 0) {
+        buttonNo.Top += buttonSpeed;
+      } else if (buttonNoDimensionY <= 0 && buttonNoDimensionY + 90 >= 0 && Math.Abs(buttonNoDimensionX) - 104 <= 0) {
+        buttonNo.Top -= buttonSpeed;
+      }
+    }
+
+    private void Form1_MouseMove(object sender, MouseEventArgs e) {
+      MoveButtonNo();
+    }
+
+    private void buttonYes_MouseMove(object sender, EventArgs e) {
+      MoveButtonNo();
+    }
+
+    private void textBoxForCheatCode_MouseMove(object sender, EventArgs e) {
+      MoveButtonNo();
+    }
+
+    private void IsCheatCodeActivated() {
+      ButtonStop = (InputKeys.Contains(SuperSecretCheatCode));
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e) {
+      InputKeys = textBoxForCheatCode.Text;
+      IsCheatCodeActivated();
+    }
+
+    private void buttonYes_Click(object sender, EventArgs e) {
+      MessageBox.Show("Oh, really?", "AHAHAH YOU ARE FOOL!!");
+    }
+
+    private void buttonNo_Click(object sender, EventArgs e) {
+      if (ButtonStop) {
+        MessageBox.Show("WOWOW YOU'RE NOT FOOL!!!", "MLG 360 PRO GAMER");
+      }
+    }
+  }
+}
